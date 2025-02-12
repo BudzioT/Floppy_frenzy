f2ff7dee38e684c1eb58ba11ef078a14ceb7d89e
BudzioT <budzio.zsm1@gmail.com>
2025-02-12 18:09:06 +0100

    I guess ragdoll kinda works

--- scripts/entities/default_character.gd
+++ scripts/entities/default_character.gd
@@ -1,27 +1,33 @@
-extends CharacterBody3D
+extends Node3D
 
-var target_velocity = Vector3.ZERO
-@export var speed = 14
+var ragdoll_mode: bool = false
 
-func _ready():
-	$Skeleton3D/PhysicalBoneSimulator3D.physical_bones_start_simulation()
+@export var spring_stiffness: float = 4.0
+@export var spring_damping: float = 80.0
+@export var max_angular_force: float = 9999.0
 
-func _physics_process(delta: float) -> void:
-	var dir = Vector3.ZERO
-	if Input.is_action_pressed("move_forwards"):
-		dir.z += 1
-	if Input.is_action_pressed("move_backwards"):
-		dir.z -= 1
-	if Input.is_action_pressed("move_left"):
-		dir.x -= 1
-	if Input.is_action_pressed("move_right"):
-		dir.x += 1
-		
-	if dir != Vector3.ZERO:
-		dir = dir.normalized()
-		
-	target_velocity.x = dir.x * speed
-	target_velocity.z = dir.z * speed
+@onready var physical_skeleton: Skeleton3D = $Physical/Armature/Skeleton3D
+@onready var animated_skeleton: Skeleton3D = $Animated/Armature/Skeleton3D
+var physical_bones: Array = []
+
+var delta: float = 0.0
+
+func _ready() -> void:
+	# Activate ragdoll, get all bones
+	physical_skeleton.physical_bones_start_simulation()
+	physical_bones = physical_skeleton.get_children().filter(func(x): return x is PhysicalBone3D)
+
+func _on_skeleton_3d_skeleton_updated() -> void:
+	if not ragdoll_mode:
+		for bone: PhysicalBone3D in physical_bones:
+			var current_transform: Transform3D = physical_skeleton.global_transform * physical_skeleton.get_bone_global_pose(bone.get_bone_id())
+			var target_transform: Transform3D = animated_skeleton.global_transform * animated_skeleton.get_bone_global_pose(bone.get_bone_id())
+			var diff: Basis = (target_transform.basis * current_transform.basis.inverse())
+			var torque = hookes_law(diff.get_euler(), bone.angular_velocity, spring_stiffness, spring_damping)
+			torque = torque.limit_length(max_angular_force)
+			
+			bone.angular_velocity += torque * delta
+			
+func hookes_law(displacement: Vector3, current_velocity: Vector3, stiffnes: float, damping: float) -> Vector3:
+	return (stiffnes * displacement) - (damping * current_velocity)
 	
-	velocity = target_velocity
-	move_and_slide()
