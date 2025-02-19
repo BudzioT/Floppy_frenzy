a15b327cd7672a617658724796369c108d317d0d
BudzioT <budzio.zsm1@gmail.com>
2025-02-19 10:35:11 +0100

    Tried spawning, ig kinda works?

--- scripts/utils/sceneManager.gd
+++ scripts/utils/sceneManager.gd
@@ -1,11 +1,16 @@
 extends Node3D
 
+@export var player_scene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var index: int = 0
+	for player in GameManager.players:
+		var current_player = player_scene.instantiate()
+		add_child(current_player)
+		
+		for spawn in get_tree().get_nodes_in_group("SpawnLocations"):
+			if spawn.name == ("Spawn" + str(index)):
+				print("HIT")
+				current_player.global_transform.origin = spawn.global_transform.origin
+		index += 1
